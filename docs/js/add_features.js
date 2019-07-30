95fd9222319998d19e3558f64242e028bbc56464
mhamilt <matt.hamilton@ed.ac.uk>
2019-07-30 14:11:38 +0100

    add: map features in layer for points, mouse functions and autocomplete script first draft

--- docs/js/add_features.js
+++ docs/js/add_features.js
@@ -1,5 +1,6 @@
+//==============================================================================
 var colors = ['#595', '#C33', '#358', '#449']
-
+//==============================================================================
 function addFeatures()
 {
   var url = [
@@ -7,53 +8,63 @@ function addFeatures()
     "https://raw.githubusercontent.com/Edinburgh-College-of-Art/sample-data-sets/master/geojson/edinburgh_campus/fascilities/workshops.json"
   ];
 
-
-  for (var i = 0; i < url.length; i++)
-  {
-    $.getJSON(url[i], function(data)
-    {
-      for (var i = 0; i < data.features.length; i++)
-      {
-        switch (data.features[i].geometry.type)
-        {
-          case "Point":
-            addPoint(data.features[i])
-            break;
-          case "Polygon":
-
-            addPolygon(data.features[i], i)
-            break;
-          default:
-
-        }
-
-      }
-    });
-  }
+  addWorkshops(url[1]);
 }
+//==============================================================================
 
-
-function addPoint(feature)
+function makeDetails(feature)
 {
   let detailsHtml = '';
   detailsHtml += '<h3>';
-  detailsHtml += (feature.properties.link) ? '<a href="' + feature.properties.link + '" target="_blank">'  + feature.properties.name + '</a>' : feature.properties.name
+  detailsHtml += (feature.properties.link) ? '<a href="' + feature.properties.link + '" target="_blank">' + feature.properties.name + '</a>' : feature.properties.name
   detailsHtml += '</h3>';
   detailsHtml += (feature.properties.room) ? '<strong> Room: ' + feature.properties.room + '</strong>' : '';
   detailsHtml += (feature.properties.notes) ? '<p>' + feature.properties.notes + '</p>' : '';
 
-  var popup = new mapboxgl.Popup().setHTML(detailsHtml);
-  var marker = new mapboxgl.Marker()
-    .setLngLat(feature.geometry.coordinates)
-    .setPopup(popup)
-    .addTo(map);
-};
+  return detailsHtml;
+}
+//==============================================================================
+function addWorkshops(url)
+{
+  $.getJSON(url, function(json)
+  {
+    map.addLayer(
+    {
+      "id": "workshops",
+      "type": "symbol",
+      "source":
+      {
+        "type": "geojson",
+        "data": json
+      },
+      "layout":
+      {
+        "icon-image": "marker-11",
+        "icon-padding": 0,
+        "icon-offset": [0, -5],
+        "icon-size": 3,
+        "icon-allow-overlap": true
+      }
+    });
 
+    json.features.forEach(function(feature)
+    {
+      var popup = new mapboxgl.Popup().setHTML(makeDetails(feature));
+      var marker = new mapboxgl.Marker()
+        .setLngLat(feature.geometry.coordinates)
+        .setPopup(popup)
+        .addTo(map);
+      markers.push(marker)
+    });
+
+    console.log(markers);
+  })
+};
+//==============================================================================
 function addPolygon(feature, i)
 {
   if (feature.properties.name)
   {
-    console.log(feature.properties.name);
     map.addLayer(
     {
       'id': feature.properties.name,
@@ -73,3 +84,4 @@ function addPolygon(feature, i)
     });
   }
 };
+//==============================================================================
--- docs/js/map.js
+++ docs/js/map.js
@@ -2,18 +2,20 @@ var ecaLauristonCoordinates = [-3.1987, 55.9456];
 var nicePitch = 55;
 var niceBearing = 33;
 var niceZoom = 17;
+var markers = [];
 //==============================================================================
 mapboxgl.accessToken = 'pk.eyJ1IjoibWhhbWlsdCIsImEiOiJjamgwbTNzMjIwYjlnMnlsY3pmYmpmaXF4In0.I0MFX6B37frj28B5qkhp9g';
-var map = new mapboxgl.Map({
-    style: 'mapbox://styles/mapbox/light-v9',
-    center: ecaLauristonCoordinates,
-    zoom: niceZoom,
-    // pitch: nicePitch,
-    // bearing: niceBearing,
-    // maxBounds: bounds,
-    // interactive: false,
-    // hash: false,
-    container: 'map'
+var map = new mapboxgl.Map(
+{
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: ecaLauristonCoordinates,
+  zoom: niceZoom,
+  // pitch: nicePitch,
+  // bearing: niceBearing,
+  // maxBounds: bounds,
+  // interactive: false,
+  // hash: false,
+  container: 'map'
 });
 map.addControl(new mapboxgl.FullscreenControl());
 // var geocoder = new MapboxGeocoder(
@@ -25,28 +27,97 @@ map.addControl(new mapboxgl.FullscreenControl());
 // document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
 // Add geolocate control to the map.
-map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: false
-    },
-    trackUserLocation: false
+map.addControl(new mapboxgl.GeolocateControl(
+{
+  positionOptions:
+  {
+    enableHighAccuracy: false
+  },
+  trackUserLocation: false
 }));
 
 //==============================================================================
+var search_element = document.getElementById('geocoder');
+var feature_list = [];
+//==============================================================================
+function renderListings(features)
+{
+  // listingEl.innerHTML = ''; // Clear any existing listings
+  if (features.length)
+  {
+    // features.forEach(function(feature)
+    // {
+    //   var prop = feature.properties;
+    //   let popup = new mapboxgl.Popup(
+    //   {
+    //     closeButton: false
+    //   });
+    //   popup.setLngLat(feature.geometry.coordinates)
+    //     .setText(feature.properties.name + ' (' + feature.properties.abbrev + ')')
+    //     .addTo(map);
+  };
+  // listingEl.appendChild(item);
+};
+
 // The 'building' layer in the mapbox-streets vector source contains building-height
 // data from OpenStreetMap.
-map.on('load', function() {
-    // Insert the layer beneath any symbol layer.
-    var layers = map.getStyle().layers;
-
-    var labelLayerId;
-    for (var i = 0; i < layers.length; i++) {
-        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-            labelLayerId = layers[i].id;
-            break;
-        }
-    }
-
+map.on('load', function()
+{
   addFeatures();
+  // Insert the layer beneath any symbol layer.
+  // var layers = map.getStyle().layers;
+  //
+  // var labelLayerId;
+  // for (var i = 0; i < layers.length; i++)
+  // {
+  //   if (layers[i].type === 'symbol' && layers[i].layout['text-field'])
+  //   {
+  //     labelLayerId = layers[i].id;
+  //     break;
+  //   }
+  // }  
+  // console.log(layer);
+  // if (features)
+  // {
+  //   var uniqueFeatures = getUniqueFeatures(features, "iata_code");
+  //   // Populate features for the listing overlay.
+  //   renderListings(uniqueFeatures);
+  //
+  //   // Clear the input container
+  //   filterEl.value = '';
+  //
+  //   // Store the current features in sn `airports` variable to
+  //   // later use for filtering on `keyup`.
+  //   airports = uniqueFeatures;
+  // }
+  //----------------------------------------------------------------------------
+  // Search Bar
+  search_element.addEventListener('keyup', function(e) {});
+  //----------------------------------------------------------------------------
+  // Mouse Functions
+  map.on('moveend', function()
+  {
+    var features = map.queryRenderedFeatures(
+    {
+      layers: ['workshops']
+    });
+    console.log(features);
+  });
+
+  map.on('mousedown', function()
+  {
+    for (marker of markers)
+    {
+      marker.remove();
+    }
+  });
+
+  map.on('mouseup', function()
+  {
+    for (marker of markers)
+    {
+      marker.addTo(map);
+    }
+  });
 
 });
